779b7c919b18a3ff88767ce6fbf3bf0b3eaac426
Maxim Litvyakov <maxim.litvyakov@sibers.com>
2022-01-03 23:10:35 +0700

    Added admin auth

---
+++ src/Controller/Admin/SecurityController.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class SecurityController
+ *
+ * @Route("/admin")
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="admin_security_login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     *
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+//        if ($this->getUser()) {
+//            return $this->redirectToRoute('main_profile');
+//        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="admin_security_logout")
+     */
+    public function logout(): RedirectResponse
+    {
+        return $this->redirectToRoute('admin_security_login');
+    }
+}
